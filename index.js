0a12c97870304456c831d557365bae55a0d04047
WendsonMagalhaes <40031617+WendsonMagalhaes@users.noreply.github.com>
2024-07-03 19:51:52 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,27 +4,19 @@ const bodyParser = require('body-parser');
 const { getAuthSheets } = require('./auth'); // Importe a função getAuthSheets do seu arquivo de autenticação
 const router = express.Router();
 
-
-const port = 3032;
-const host = '0.0.0.0';
+const port = 3030;
 var path = require('path');
 const app = express();
+
 var loginAdriano = "adriano";
-var passwordAdriano = "762018";
+var passwordAdriano = "1111";
 var loginDurval = "durval";
-var passwordDurval = "142027";
+var passwordDurval = "2222";
 var loginOscar = "oscar";
-var passwordOscar = "230277";
-var loginWendson = "wendson";
-var passwordWendson = "4444";
+var passwordOscar = "3333";
 var usuario;
 
-app.use(session({
-    secret: '09r78cn82b3r89x1@38xy4184',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false }
-}));
+app.use(session({ secret: '09r78cn82b3r89x1@38xy4184' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -35,67 +27,28 @@ app.set('views', path.join(__dirname, '/views'));
 app.use('/', router); // Use o roteador express
 
 
-// Inicia o servidor
-app.listen(port, host, () => {
-    console.log(`Server running at http://172.30.5.4:${port}`);
+app.listen(process.env.PORT || port, () => {
+    console.log('SERVIDOR RODANDO')
 });
-
-
-app.use(express.static(path.join(__dirname, 'www')));
-app.use('/lib', express.static(path.join(__dirname, 'lib')));
-app.use('/css', express.static(path.join(__dirname, 'css')));
-
-
 
 app.post('/', (req, res) => {
-    let errorMessage = ""; // Inicializa a mensagem de erro como uma string vazia
-
-    if (req.body.password == passwordAdriano && req.body.login.toLowerCase().trim() == loginAdriano) {
+    if (req.body.password == passwordAdriano && req.body.login.toLowerCase() == loginAdriano) {
         req.session.login = loginAdriano;
-        usuario = loginAdriano.charAt(0).toUpperCase() + loginAdriano.slice(1).toLowerCase();
+        usuario = loginAdriano;
         res.render('home', { login: usuario });
-
-    } else if (req.body.password == passwordDurval && req.body.login.toLowerCase().trim() == loginDurval) {
+    } else if (req.body.password == passwordDurval && req.body.login.toLowerCase() == loginDurval) {
         req.session.login = loginDurval;
-        usuario = loginDurval.charAt(0).toUpperCase() + loginDurval.slice(1).toLowerCase();
+        usuario = loginDurval
         res.render('home', { login: usuario });
     }
-    else if (req.body.password == passwordOscar && req.body.login.toLowerCase().trim() == loginOscar) {
+    else if (req.body.password == passwordOscar && req.body.login.toLowerCase() == loginOscar) {
         req.session.login = loginOscar;
-        usuario = loginOscar.charAt(0).toUpperCase() + loginOscar.slice(1).toLowerCase();
+        usuario = loginOscar
         res.render('home', { login: usuario });
-    } else if (req.body.password == passwordWendson && req.body.login.toLowerCase().trim() == loginWendson) {
-        req.session.login = loginWendson;
-        usuario = loginWendson.charAt(0).toUpperCase() + loginWendson.slice(1).toLowerCase();
-        res.render('controle-registros', { login: usuario });
-    }
-    else {
-        errorMessage = "Login ou senha incorretos. Por favor, tente novamente."; // Define a mensagem de erro
-        res.render('index', { errorMessage: errorMessage }); // Passa a mensagem de erro para o modelo
-    }
-
-});
-// Middleware para proteger rotas que requerem autenticação
-function checkAuth(req, res, next) {
-    if (req.session && req.session.login) {
-        next();
     } else {
-        res.redirect('/');
+        res.render('index');
     }
-}
 
-// Rota protegida de exemplo
-app.get('/home', checkAuth, (req, res) => {
-    const usuario = req.session.login.charAt(0).toUpperCase() + req.session.login.slice(1).toLowerCase();
-    res.render('home', { login: usuario });
-});
-app.get('/', (req, res) => {
-    const errorMessage = req.query.error || ""; // Obtém a mensagem de erro da query string ou define como uma string vazia se não houver erro
-    res.render('index', { errorMessage: errorMessage });
-});
-
-app.get('/', (req, res) => {
-    res.render('index', { errorMessage: "" }); // Passa uma mensagem de erro vazia para o modelo
 });
 app.get('/', (req, res) => {
     if (req.session.login) {
@@ -106,21 +59,6 @@ app.get('/', (req, res) => {
     }
 
 });
-
-app.get('/registrados', (req, res) => {
-    if (req.session.login) {
-        res.render('registrados', { login: usuario });
-    } else {
-        res.render('index');
-    }
-
-});
-app.get('/home', (req, res) => {
-    res.render('home', { login: usuario });
-});
-
-
-
 
 app.post("/addRow", async (req, res) => {
     try {
@@ -152,18 +90,6 @@ app.post("/addRow", async (req, res) => {
         res.status(500).send('Erro interno do servidor');
     }
 });
-app.use(express.static('css', {
-    setHeaders: (res, path) => {
-        if (path.endsWith('.css')) {
-            res.set('Content-Type', 'text/css');
-        }
-    }
-}));
-// Serve arquivos estáticos do diretório 'js'
-app.use(express.static(path.join(__dirname, 'js'), {
-    setHeaders: (res, filePath) => {
-        if (filePath.endsWith('.js')) {
-            res.set('Content-Type', 'text/javascript');
-        }
-    }
-}));
+
+
+  
